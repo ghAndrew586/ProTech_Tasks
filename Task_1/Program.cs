01a408a035efc1e46d5a18351229afaf707df94a
Andrey Kochetov <and-usmail-586@yandex.ru>
2024-02-05 21:05:44 +0500

    loop-program

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -24,6 +24,18 @@ namespace Task_1
                 var firstSegment = new ArraySegment<char>(mainLine, mainLine.Length / 2, mainLine.Length / 2);
                 Console.Write(String.Join("", firstSegment)); Console.WriteLine(String.Join("", lastSegment));
             }
+
+            Console.Write(" . . . Нажмите любую кнопку, чтобы выйти; Space, чтобы перезапустить  . . . ");
+            char endKey = Console.ReadKey().KeyChar;
+
+            if (endKey == ' ')
+            {
+                System.Diagnostics.Process.Start(Assembly.GetExecutingAssembly().Location);
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
         }
     }
 }
