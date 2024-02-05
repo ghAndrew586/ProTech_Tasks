04448fce78fbecef140eebc9509a681096dd7804
Andrey Kochetov <and-usmail-586@yandex.ru>
2024-02-05 21:04:09 +0500

    program-main-logic

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Task_1
 {
@@ -10,6 +7,23 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите строку: ");
+            char[] mainLine = Console.ReadLine().ToCharArray();
+
+            Console.Write("Выход: ");
+            if (mainLine.Length % 2 != 0)
+            {
+                string unrevLine = new string(mainLine);
+                Array.Reverse(mainLine);
+                Console.Write(mainLine); Console.WriteLine(unrevLine);
+            }
+            else
+            {
+                Array.Reverse(mainLine);
+                var lastSegment = new ArraySegment<char>(mainLine, 0, mainLine.Length / 2);
+                var firstSegment = new ArraySegment<char>(mainLine, mainLine.Length / 2, mainLine.Length / 2);
+                Console.Write(String.Join("", firstSegment)); Console.WriteLine(String.Join("", lastSegment));
+            }
         }
     }
 }
